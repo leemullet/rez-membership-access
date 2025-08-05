34107289c56288bc3564a7ba71ac4e87550bfbd4
leemullet <135128094+leemullet@users.noreply.github.com>
2025-08-05 18:26:51 -0400

    Update challenge.js

--- api/challenge.js
+++ api/challenge.js
@@ -1,6 +1,11 @@
 import { createChallenge } from 'altcha-lib';
 
 export default async function handler(req, res) {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   // Handle CORS preflight
   if (req.method === 'OPTIONS') {
     res.status(200).end();
