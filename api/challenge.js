47a0c2099caf9d24556fb33f9a15dc9167b25e48
leemullet <135128094+leemullet@users.noreply.github.com>
2025-08-05 16:48:18 -0400

    Update challenge.js

--- api/challenge.js
+++ api/challenge.js
@@ -4,7 +4,7 @@ module.exports = async function handler(req, res) {
   // Set comprehensive CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, HEAD');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+  res.setHeader('Access-Control-Allow-Headers', '*');
   res.setHeader('Access-Control-Max-Age', '86400'); // Cache preflight for 24 hours
   
   // Handle preflight OPTIONS request
